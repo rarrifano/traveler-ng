444d3bcdbc21f5706cc7cc7a91ef22821e848929
Cesar Augusto de Carvalho Silva <CIANDT\cesars@lnb028195cps.ciandt.global>
2020-12-12 11:07:17 -0300

    usuario form implementation

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { UsuarioListComponent } from './usuario/usuario-list/usuario-list.component';
 import { NotaListComponent } from './nota/nota-list/nota-list.component';
 import { NotaFormComponent } from './nota/nota-form/nota-form.component';
+import { UsuarioFormComponent } from './usuario/usuario-form/usuario-form.component';
 
 const routes: Routes = [
-  {path:'nota', component: NotaListComponent},
-  {path:'nota/novo', component: NotaFormComponent},
-  {path:'nota/:id', component: NotaFormComponent},
+  { path:'nota', component: NotaListComponent },
+  { path:'nota/novo', component: NotaFormComponent  },
+  { path:'nota/:id', component: NotaFormComponent },
   { path: 'usuario', component: UsuarioListComponent },
+  { path:'usuario/novo', component: UsuarioFormComponent  },
+  { path:'usuario/:id', component: UsuarioFormComponent },
 ];
 
 @NgModule({
